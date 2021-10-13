f994c667579cbef7b05cb8b17a7f877133c94c3b
jferrergil <jferrergil1@gmail.com>
2021-10-13 11:41:42 +0200

    pasamos test creando archivo

--- function.js
+++ function.js
@@ -1,13 +1,13 @@
 function strictEquals(a, b) {
+  let result = Object.is(a, b);
   if (Number.isNaN(a) && Number.isNaN(b)) {
-    return false;
+    result = false;
   } else if (
     (Object.is(a, 0) && Object.is(b, -0)) ||
     (Object.is(a, -0) && Object.is(b, 0))
   ) {
-    return true;
-  } else {
-    return Object.is(a, b);
+    result = true;
   }
+  return result;
 }
 console.log(strictEquals("a", "b"));
